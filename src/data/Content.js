629718a0b9b4563c03afa2992d201b18f4c7f25a
Aniket Yadav <aniketyadav690@gmail.com>
2023-09-18 21:44:52 +0530

    content section added

---
+++ src/data/Content.js
@@ -0,0 +1,48 @@
+export const heroOne = {
+  reverse: true,
+  inverse: true,
+  topLine: {
+    text: "Founded in 2021",
+  },
+  headline: "Anti-bot bypass with AI",
+  description:
+    "Make 10 million requests if you wish, nobody will notice you're a scraper. The mornings with Access Denied screens are over for you with the best web scraping API. Headless Browser Rotating Proxies Anti-CAPTCHA JS Challenge Solver",
+  buttonLabel: "Scrape Everything Now",
+  imgStart: "start",
+  img: "https://cdn.zenrows.com/images/home/builtin-antibot.png",
+  start: "true",
+};
+
+export const heroTwo = {
+  reverse: false,
+  inverse: false,
+  topLine: {
+    text: "Designed & Developed",
+  },
+  headline: "The web scraping API Swiss knife",
+  description:
+    "Scraping with login? Clicking on elements? Add human interactivity like you had an army at your command.",
+  buttonLabel: "Try Javascript Rendering Myself",
+
+  linkTo: "/more",
+  imgStart: "start",
+  img: "https://cdn.zenrows.com/images/home/builtin-antibot.png",
+  start: "true",
+};
+
+export const heroThree = {
+  reverse: true,
+  inverse: true,
+  topLine: {
+    text: "Highly reputed brand",
+  },
+  headline: "The data is looking for you ",
+  description:
+    "Instead of digging into the HTML and be updating your code, try the auto-parsing magic for most popular websites. You'll get a JSON with the data you care about.",
+  buttonLabel: "Yes Give me that!",
+
+  linkTo: "/download",
+  imgStart: "",
+  img: "https://cdn.zenrows.com/images/home/js-rendering.png",
+  start: "true",
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,18 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Review from "../Components/Review/Review";
 import Faq from "../Components/Faq/Faq";
+import Content from "../Components/Content/Content";
+import { heroOne, heroThree, heroTwo } from "../data/Content";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Review />
-       <Faq />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
+      <Faq />
     </>
   );
 };
